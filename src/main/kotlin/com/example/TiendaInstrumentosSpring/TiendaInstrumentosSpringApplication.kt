3fdfb5f01f3e32fc78b21391d26f950dd8511dfc
Kriol368 <kriol368@gmail.com>
2026-01-12 16:45:39 +0100

    Initial commit

---
+++ src/main/kotlin/com/example/TiendaInstrumentosSpring/TiendaInstrumentosSpringApplication.kt
@@ -0,0 +1,11 @@
+package com.example.TiendaInstrumentosSpring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TiendaInstrumentosSpringApplication
+
+fun main(args: Array<String>) {
+	runApplication<TiendaInstrumentosSpringApplication>(*args)
+}
